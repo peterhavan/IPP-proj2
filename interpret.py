74cd589d33e0796971dacd5481e86d65afa15605
Sufu <sufu@sufu-X555LF>
2018-04-12 18:16:20 +0200

    first lines

--- interpret.py
+++ interpret.py
@@ -2,10 +2,114 @@
 import xml.etree.ElementTree as ET
 import argparse
 
-def main():
+def getOptions():
     parser = argparse.ArgumentParser()
-    parser.parse_args()
-    parser.add_argument("--source", help="source file")
+    parser.add_argument("--source", required=True, help="source file") #required=True
+    args = parser.parse_args()
+    return args.source
+    
+
+def main():
+    filename = getOptions()
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    instList = list()
+    frameStack = variableTable()     
+    
+    for instructionXML in root:
+           # print(instructionXML.tag, instructionXML.attrib)
+            instList.append(getInst(instructionXML, frameStack))
+            #for argumentXML in instructionXML:
+            #   print(argumentXML.text, argumentXML.attrib)
+           # print("--------")
+    for i in instList:
+        print(i.name)
+        print(i.args)
+        print("---------")
+        
+    interpret(frameStack, instList)
+            
+def getInst(instructionXML, frameStack):
+    argList = list()
+    for argumentXML in instructionXML:
+        argList.append(argumentXML.text)
+    opcode = instructionXML.attrib.get("opcode")
+    
+    inst = Instruction(opcode, argList)
+    return inst
+    
+def interpret(frameStack, instList):
+    for instruction in instList:
+        if instruction.name == "DEFVAR":
+            frameStack.addVariable(instruction.args[0])
+        
+        
+    
+    
+        
+class Frame:
+    def __init__(self):
+        self.variables = dict()
+        
+   # def addVariable(self, variableName):
+    #    self.variables.get(variableName, "not init")
+        
+class variableTable:
+    def __init__(self):
+        self.GF = Frame()
+        self.LF = list()
+        self.TF = Frame()
+    
+    def addVariable(self, arg):
+        frameName = arg.split('@',1)[0]
+        if frameName == "GF":
+            self.GF.variables.get(arg.split('@',1)[1], "not init")
+            #self.GF.addVariable(arg.split('@',1)[1])
+        elif frameName == "LF":
+            if not self.LF:
+                exit(55)
+            else:
+                self.LF.variables.get(arg.split('@',1)[1], "not init")
+                #self.LF.addVariable(arg.split('@',1)[1])
+        elif frameName == "TF":
+            self.TF.variables.get(arg.split('@',1)[1], "not init")
+            #self.TF.addVariable(arg.split('@',1)[1])
+            
+    def initVariable(self, name, value):
+        frameName = name.split('@',1)[0]
+        variableName = name.split('@',1)[1]
+        
+        if frameName == "GF":
+            if variableName not in self.GF.variables.keys():
+                exit(54)
+            else:
+                self.GF.variables.update({variableName : value})
+                
+        elif frameName == "LF":
+            if not self.LF:
+                exit(55)
+            elif variableName not in self.LF.variables.keys():
+                exit(54)
+            else:
+                self.LF.variables.update({variableName : value})
+                
+        elif frameName == "TF":
+            if variableName not in self.TF.variables.keys():
+                exit(54)
+            else:
+                self.TF.variables.update({variableName : value})
+            
+            
+        
+                   
+class Instruction:
+    def __init__(self, name, args):
+        self.name = name
+        self.args = args
+        
+        
+    
     
 if __name__ == '__main__':
     main()
+    
