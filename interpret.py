4c147a0990753fd8e1318956381748fbc8e28c88
Sufu <sufu@sufu-X555LF>
2018-04-12 12:35:08 +0200

    setting up

--- interpret.py
+++ interpret.py
@@ -5,6 +5,7 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.parse_args()
+    parser.add_argument("--source", help="source file")
     
 if __name__ == '__main__':
     main()
